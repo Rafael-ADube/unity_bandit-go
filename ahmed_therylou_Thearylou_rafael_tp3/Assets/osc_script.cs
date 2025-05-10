ffeff02997504d37762aca5620bb246fe553c131
Rafael-ADube <124070431+Rafael-ADube@users.noreply.github.com>
2025-05-09 20:47:34 -0400

    rafael_configuration_OSC_9mai

---
+++ ahmed_therylou_Thearylou_rafael_tp3/Assets/osc_script.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using extOSC;
+public class osc_script : MonoBehaviour
+{
+    public OSCReceiver oscReceiver;
+    public OSCTransmitter oscTransmitter;
+}
