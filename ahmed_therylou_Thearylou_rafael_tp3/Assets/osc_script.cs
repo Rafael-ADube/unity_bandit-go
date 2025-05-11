81317faf76bd087c906a78c8c29fce3be07a9473
Rafael-ADube <124070431+Rafael-ADube@users.noreply.github.com>
2025-05-10 22:35:54 -0400

    rafael_corrige_bug10Mai

--- ahmed_therylou_Thearylou_rafael_tp3/Assets/osc_script.cs
+++ ahmed_therylou_Thearylou_rafael_tp3/Assets/osc_script.cs
@@ -1,9 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using extOSC;
-public class osc_script : MonoBehaviour
+
+public class OSCSpawnControl : MonoBehaviour
 {
-    public OSCReceiver oscReceiver;
-    public OSCTransmitter oscTransmitter;
+    public OSCReceiver receiver;
+    public string oscAddress = "/spawnrate";
+
+    public static float spawnInterval = 3f;
+
+    void Start()
+    {
+        receiver.Bind(oscAddress, OnSpawnIntervalReceived);
+    }
+
+    void OnSpawnIntervalReceived(OSCMessage message)
+    {
+        if (message.ToFloat(out float interval))
+        {
+            spawnInterval = Mathf.Clamp(interval, 0.1f, 10f);
+            Debug.Log("Intervalle de spawn mis à jour via OSC : " + spawnInterval);
+        }
+    }
 }
